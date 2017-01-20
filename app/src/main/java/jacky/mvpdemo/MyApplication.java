7552cc21a47e452918cbf63e75b5251ba28c36a8
1507-PC\1507 <353938360@qq.com>
2017-01-20 17:13:47 +0800

    加入greendao  和 MD dialog

--- app/src/main/java/jacky/mvpdemo/MyApplication.java
+++ app/src/main/java/jacky/mvpdemo/MyApplication.java
@@ -1,17 +1,44 @@
 package jacky.mvpdemo;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.blankj.utilcode.utils.Utils;
+import com.yuan.leopardkit.db.dao.DaoMaster;
+import com.yuan.leopardkit.db.dao.DaoSession;
+
+import jacky.mvpdemo.bean.Shop;
 
 /**
  * Created by 1507 on 2017/1/13.
  */
 
 public class MyApplication extends Application {
+    private static DaoSession daoSession;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Utils.init(getApplicationContext());
+        setupDatabase();
+    }
+
+    /**
+     * 配置数据库
+     */
+    private void setupDatabase() {
+        //创建数据库shop.db"
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "user.db", null);
+        //获取可写数据库
+        SQLiteDatabase db = helper.getWritableDatabase();
+        //获取数据库对象
+        DaoMaster daoMaster = new DaoMaster(db);
+        //获取Dao对象管理者
+        daoSession = daoMaster.newSession();
+
+    }
+
+    public static DaoSession getDaoInstant() {
+        return daoSession;
     }
 }
--- app/src/main/java/jacky/mvpdemo/ui/activity/MainActivity.java
+++ app/src/main/java/jacky/mvpdemo/ui/activity/MainActivity.java
@@ -1,15 +1,97 @@
 package jacky.mvpdemo.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import jacky.mvpdemo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Button btn_1, btn_2, btn_3, btn_4;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(context)
+                        .title(R.string.title)
+                        .content(R.string.content)
+                        .positiveText(R.string.agree)
+                        .negativeText(R.string.disagree)
+                        .show();
+            }
+        });
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(context)
+                        .iconRes(R.drawable.md_btn_selected)
+                        .limitIconToDefaultSize()
+                        .title(R.string.content)
+                        .positiveText(R.string.agree)
+                        .negativeText(R.string.disagree)
+                        .onAny(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                Toast.makeText(context, "Prompt checked? " + dialog.isPromptCheckBoxChecked(), Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .checkBoxPromptRes(R.string.dont_ask_again, false, null)
+                        .show();
+            }
+        });
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(context)
+                        .title(R.string.title)
+                        .items(R.array.items)
+                        .itemsCallback(new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                            }
+                        })
+                        .show();
+            }
+        });
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(context)
+                        .title(R.string.title)
+                        .items(R.array.items)
+                        .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallbackSingleChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                                /**
+                                 * If you use alwaysCallSingleChoiceCallback(), which is discussed below,
+                                 * returning false here won't allow the newly selected radio button to actually be selected.
+                                 **/
+                                return true;
+                            }
+                        })
+                        .positiveText(R.string.choose)
+                        .show();
+            }
+        });
+
     }
+
 }
